28dcd7a882553665a6b3d792e8d00864d5fc9058
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2014-01-04 15:49:57 -0500

    Fix stats controller where clause for PG

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -5,13 +5,13 @@ class StatsController < ApplicationController
     School.all.each { |s| @applied[s.name.to_sym] = s.hackers.count }
 
     @accepted = Hash.new;
-    School.all.each { |s| @accepted[s.name.to_sym] = s.hackers.where("status IS 'Accepted'").count }
+    School.all.each { |s| @accepted[s.name.to_sym] = s.hackers.where("status = 'Accepted'").count }
     
     @waitlisted = Hash.new;
-    School.all.each { |s| @waitlisted[s.name.to_sym] = s.hackers.where("status IS 'Waitlisted'").count }
+    School.all.each { |s| @waitlisted[s.name.to_sym] = s.hackers.where("status = 'Waitlisted'").count }
     
     @rejected = Hash.new;
-    School.all.each { |s| @rejected[s.name.to_sym] = s.hackers.where("status IS 'Rejected'").count }
+    School.all.each { |s| @rejected[s.name.to_sym] = s.hackers.where("status = 'Rejected'").count }
     
   end
 end
