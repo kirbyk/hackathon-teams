8367b9b8dfd9810b17487de6739742717da7d2ac
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2014-01-05 13:19:30 -0500

    More stats and a link

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -5,13 +5,32 @@ class StatsController < ApplicationController
     School.all.each { |s| @applied[s.name.to_sym] = s.hackers.count }
 
     @accepted = Hash.new;
-    School.all.each { |s| @accepted[s.name.to_sym] = s.hackers.where("status = 'Accepted'").count }
+    accepted_id = Status.find_by_name('Accepted').id
+    School.all.each { |s| @accepted[s.name.to_sym] = s.hackers.where("status_id = #{accepted_id}").count }
     
     @waitlisted = Hash.new;
-    School.all.each { |s| @waitlisted[s.name.to_sym] = s.hackers.where("status = 'Waitlisted'").count }
+    waitlisted_id = Status.find_by_name('Waitlisted').id
+    School.all.each { |s| @waitlisted[s.name.to_sym] = s.hackers.where("status_id = #{waitlisted_id}").count }
     
     @rejected = Hash.new;
-    School.all.each { |s| @rejected[s.name.to_sym] = s.hackers.where("status = 'Rejected'").count }
+    rejected_id = Status.find_by_name('Rejected').id
+    School.all.each { |s| @rejected[s.name.to_sym] = s.hackers.where("status_id = #{rejected_id}").count }
     
+    @commited = Hash.new;
+    commited_id = Status.find_by_name('Commited').id
+    School.all.each { |s| @commited[s.name.to_sym] = s.hackers.where("status_id = #{commited_id}").count }
+    
+    @declined = Hash.new;
+    declined_id = Status.find_by_name('Declined').id
+    School.all.each { |s| @declined[s.name.to_sym] = s.hackers.where("status_id = #{declined_id}").count }
+    
+    @total = Hash.new
+    @total[:applied] = Hacker.all.count
+    @total[:accepted] = Hacker.where("status_id = #{accepted_id}").count
+    @total[:waitlisted] = Hacker.where("status_id = #{waitlisted_id}").count
+    @total[:rejected] = Hacker.where("status_id = #{rejected_id}").count
+    @total[:commited] = Hacker.where("status_id = #{commited_id}").count
+    @total[:declined] = Hacker.where("status_id = #{declined_id}").count
+
   end
 end
