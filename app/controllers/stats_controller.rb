7b15ed1c5005eb7fe4073ab0d52ede68463aaee3
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2014-01-05 16:14:43 -0500

    Add other to stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,36 +1,36 @@
 class StatsController < ApplicationController
   def index
     
-    @schools = [:'Purdue University',:'UIUC',:'University of Wisconsin - Madison',
-                :'Rutgers University',:'Carnegie Mellon University',
-                :'Ohio State University',:'University of Maryland',
-                :'University of Waterloo',:'University of Michigan',
-                :'Rose-Hulman Institute of Technology',:'Northwestern University',
-                :'University of Iowa']    
-
+    @schools = ['Purdue University','UIUC','University of Wisconsin - Madison',
+                'Rutgers University','Carnegie Mellon University',
+                'Ohio State University','University of Maryland',
+                'University of Waterloo','University of Michigan',
+                'Rose-Hulman Institute of Technology','Northwestern University',
+                'University of Iowa']    
+    
     @applied = Hash.new
-    School.all.each { |s| @applied[s.name.to_sym] = s.hackers.count }
-
     @accepted = Hash.new;
-    accepted_id = Status.find_by_name('Accepted').id
-    School.all.each { |s| @accepted[s.name.to_sym] = s.hackers.where("status_id = #{accepted_id}").count }
-    
     @waitlisted = Hash.new;
-    waitlisted_id = Status.find_by_name('Waitlisted').id
-    School.all.each { |s| @waitlisted[s.name.to_sym] = s.hackers.where("status_id = #{waitlisted_id}").count }
-    
     @rejected = Hash.new;
-    rejected_id = Status.find_by_name('Rejected').id
-    School.all.each { |s| @rejected[s.name.to_sym] = s.hackers.where("status_id = #{rejected_id}").count }
-    
     @commited = Hash.new;
-    commited_id = Status.find_by_name('Commited').id
-    School.all.each { |s| @commited[s.name.to_sym] = s.hackers.where("status_id = #{commited_id}").count }
-    
     @declined = Hash.new;
+
+    accepted_id = Status.find_by_name('Accepted').id
+    waitlisted_id = Status.find_by_name('Waitlisted').id
+    rejected_id = Status.find_by_name('Rejected').id
+    commited_id = Status.find_by_name('Commited').id
     declined_id = Status.find_by_name('Declined').id
-    School.all.each { |s| @declined[s.name.to_sym] = s.hackers.where("status_id = #{declined_id}").count }
-    
+
+    @schools.each do |school|
+      s = School.find_by_name(school)
+      @applied[school] = s.hackers.count
+      @accepted[school] = s.hackers.where("status_id = #{accepted_id}").count
+      @waitlisted[school] = s.hackers.where("status_id = #{waitlisted_id}").count
+      @rejected[school] = s.hackers.where("status_id = #{rejected_id}").count
+      @commited[school] = s.hackers.where("status_id = #{commited_id}").count
+      @declined[school] = s.hackers.where("status_id = #{declined_id}").count
+    end
+
     @total = Hash.new
     @total[:applied] = Hacker.all.count
     @total[:accepted] = Hacker.where("status_id = #{accepted_id}").count
@@ -40,7 +40,12 @@ class StatsController < ApplicationController
     @total[:declined] = Hacker.where("status_id = #{declined_id}").count
 
     @other = Hash.new
-    @other[:applied] = @applied.each_value.inject(:+)
+    @other[:applied] = @total[:applied] - @applied.each_value.inject(:+)
+    @other[:accepted] = @total[:accepted] - @accepted.each_value.inject(:+)
+    @other[:waitlisted] = @total[:waitlisted] - @waitlisted.each_value.inject(:+)
+    @other[:rejected] = @total[:rejected] - @rejected.each_value.inject(:+)
+    @other[:commited] = @total[:commited] - @commited.each_value.inject(:+)
+    @other[:declined] = @total[:declined] - @declined.each_value.inject(:+)
 
   end
 end
