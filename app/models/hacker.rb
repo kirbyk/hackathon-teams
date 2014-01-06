a5ee5fc9e2086ee470c2989d35a0ff9bfa2cd7ae
Spencer Brown <me@spencer.sx>
2014-01-06 18:01:02 -0500

    yeah

--- app/models/hacker.rb
+++ app/models/hacker.rb
@@ -6,8 +6,8 @@ class Hacker < ActiveRecord::Base
   has_attached_file :resume,
     :storage => :s3,
     :s3_credentials => {
-	    :bucket => "bm-resumes",
-      :access_key_id => "AKIAJXJUB7RVDVG3SAVA",
-      :secret_access_key => "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"
+	    :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
 end
