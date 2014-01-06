42fb035d234f703fbbb0df48331d701eebd66607
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2014-01-06 18:59:33 -0500

    Paperclip local 100%

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,13 +29,4 @@ HackathonTeams::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  Paperclip.options[:command_path] = "/usr/local/bin/"
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
 end
